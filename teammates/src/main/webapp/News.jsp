e9cdc6e55176bd2f2753c0f6b203da8a08f65465
shajie <s3582098@student.rmit.edu.au>
2018-08-13 16:14:29 +1000

    initial the weather in the news page

--- teammates/src/main/webapp/News.jsp
+++ teammates/src/main/webapp/News.jsp
@@ -4,24 +4,23 @@
 <t:staticPage currentPage="news">
   <main class="container">
   	<head>
-	  	<script type = "text/javascript" src = "jquery-3.2.1.min.js"></script>
-	    	<script type="text/javascript">
-	    	$(document).ready(function() {
-	    	    // This WILL work because we are listening on the 'document', 
-	    	    // for a click on an element with an ID of #test-element
-	    	    $(document).on("click","#test-element",function() {
-	    	        alert("click bound to document listening for #test-element");
-	    	    });
-
-	    	    // This will NOT work because there is no '#test-element' ... yet
-	    	    $("#test-element").on("click",function() {
-	    	        alert("click bound directly to #test-element");
-	    	    });
-
-	    	    // Create the dynamic element '#test-element'
-	    	    $('body').append('<div id="test-element">Click mee</div>');
-	    	});
-	 	   </script> 
+  		<style>
+  				html 
+  				{
+				  font-family: "Avenir Next", Helevetica, sans-serif;
+				  text-align: center;
+				}
+				
+				body {
+				  margin: 0 auto;
+				}
+				
+				h1,
+				h2  {
+				  font-weight: 300;
+				}
+  		</style>
+	  	<script type = "text/javascript" src = "jquery-3.2.1.min.js"></script> 
   	</head>
   	
     <h1 class="color-orange">
@@ -29,9 +28,9 @@
     </h1>
     </br>  
     <body> 
-    	<textarea id="demo" rows="4" cols="50"></textarea> 
-		
+		 <h1><div id="temp"></div><div id="minutely"></div></h1>
+       	 <h2><div id="location"></div></h2>
+	</body> 
 		<script type = "text/javascript" src = "NewsScript.js"></script> 
-	</body>
   </main>
 </t:staticPage>
--- teammates/src/main/webapp/NewsScript.js
+++ teammates/src/main/webapp/NewsScript.js
@@ -1,20 +1,28 @@
 /**
  * 
  */ 
-		 // Create a request variable and assign a new XMLHttpRequest object to it.
-		  	var request = new XMLHttpRequest();
-	
-		  	// Open a new connection, using the GET request on the URL endpoint
-		  	request.open('GET', 'https://samples.openweathermap.org/data/2.5/weather?id=2172797&appid=161b4720f54d9095f44a1068f54a7104', true);
-//		  	getJSON('https://samples.openweathermap.org/data/2.5/weather?id=2172797&appid=161b4720f54d9095f44a1068f54a7104');
-		  	$.getJSON()
-		  	//		  	request.onload = function () { 
-//		  		   var data = JSON.parse(this.response); 
-//		  		   document.getElementById("demo").value += data.name + "\n";  
-//		  		   document.getElementById("demo").value += data.name + "\n";  
-//		  		   document.getElementById("demo").value += data.whether.description+ "\n";  
-//		  		   document.getElementById("demo").value += data.main.temp+ "\n";  
-//		  	}
-//	
-		  	// Send request
-		  	request.send(null);
+		  //get the weather from below API 
+		  	function getWeather()
+		  	{
+		  		//set the html tag
+		  		var location = document.getElementById("location");
+		  		var apiCall = 'https://samples.openweathermap.org/data/2.5/weather?id=2172797&appid=161b4720f54d9095f44a1068f54a7104';
+		  	 	$.getJSON(apiCall, weatherCallback);
+		  	 	navigator.geolocation.getCurrentPosition(weatherCallback, error);
+			  	function weatherCallback(weatherData)
+			  	{
+			  		latitude = weatherData.coord.lat;
+			  	    longitude = weatherData.coord.lon;
+			  	    location.innerHTML =
+			  	      "Latitude is " + latitude + "° Longitude is " + longitude + "°";
+			  	    $("#temp").html(weatherData.main.temp + "° F");
+			        $("#minutely").html(weatherData.weather.description);
+			  	}
+
+			  	function error() {
+			  		location.innerHTML = "Locating...";
+			  	}
+		  	}
+		  	
+		  	getWeather();
+
