f0ae2959c5697d9b7d8cf91f85bdd80fa8ec769e
shajie <s3582098@student.rmit.edu.au>
2018-09-08 20:19:59 +1000

    add jackson and test case

---
+++ teammates/src/test/java/teammates/test/SEPT_test_case/JacksonAPITest.java
@@ -0,0 +1,23 @@
+import org.testng.annotations.Test;
+
+import com.example.Example;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+public class JacksonAPITest { 
+    @Test
+    public void testAPIAttribute() throws JsonParseException , JsonMappingException, IOException
+    {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Example weather = objectMapper.readValue(new File(System.getProperty("user.dir")+"//testdata//weatherAPI.json"), Example.class);
+        
+        assert weather.getId().equals(2172797):"The city id is wrong ";
+        assert weather.getName().equals("Cairns"):"not the correct location ";
+ 
+    }
+
+}
+
